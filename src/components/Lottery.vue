cffdc6857ad1b15a61d3cb02b6919257a0009704
lyl1988 <122391887@qq.com>
2017-07-01 03:46:16 -0700

    lottery finished

--- src/components/Lottery.vue
+++ src/components/Lottery.vue
@@ -22,11 +22,11 @@
             <div class="panel-heading  ">Prizes</div>
             <div class="panel-body ">
               <ol>
-                <li>prizeFirst</li>
-                <li>prizeSecond</li>
-                <li>prizeThird</li>
-                <li>prizeForth</li>
-                <li>prizeFifth</li>
+                <li>{{prize1}}</li>
+                <li>{{prize2}}</li>
+                <li>{{prize3}}</li>
+                <li>{{prize4}}</li>
+                <li>{{prize5}}</li>
               </ol>
             </div>
           </div>
@@ -37,7 +37,7 @@
             <div class="panel-heading  ">Winners</div>
             <div class="panel-body">
               <ol>
-                <li v-for="winner in winners">{{winner.wfid}}</li>
+                <li v-for="(winner,index) in winners" v-if="index<5">{{winner.wfid}}</li>
               </ol>
             </div>
           </div>
@@ -51,10 +51,10 @@
     <!-- Lucky Number-->
     <div class="panel panel-info" v-else>
       <div class="panel-heading ">Lottery lucky number: <span style="color:red"> 
-                <tr v-for="number in luckyNumber">
-                      <td>{{number.luckyNumber}}</td>
-                </tr>
-                </span></div>
+                    <tr v-for="number in luckyNumber">
+                          <td>{{number.luckyNumber}}</td>
+                    </tr>
+                    </span></div>
     </div>
     <!--get start-->
     <div class="panel panel-default">
@@ -77,14 +77,19 @@
           <tr>
             <th>warframe Id</th>
             <th>Lottery Number</th>
-            <th></th>
+            <th>Difference</th>
           </tr>
         </thead>
+        
         <tbody>
           <tr v-for="lottery in lotteries">
             <td>{{lottery.wfid}}</td>
             <td>{{lottery.lotteryKey}}</td>
+            <!--
             <td><span class="glyphicon glyphicon-trash" aria-hidden="true" v-on:click="removeLottery(lottery)"></span></td>
+            -->
+            <td><span >{{lottery.diff}}</span></td>
+            
           </tr>
         </tbody>
       </table>
@@ -121,69 +126,20 @@
           wfid: '',
           lotteryKey: ''
         },
+        eventEndTime: "July 1, 2017 03:44:40",
+        prize1: 'prize1',
+        prize2: 'prize2',
+        prize3: 'prize3',
+        prize4: 'prize4',
+        prize5: 'prize5',
         timeLeft: '',
         luckyNumber: 0,
         distance: 0,
-        showTimeLeft: true,
-        prize1:'',
-        prize2:'',
-        prize3:'',
-        prize4:'',
-        prize5:''
-
-
+        showTimeLeft: true
       }
     },
     methods: {
       addLottery: function() {
-
-
-        var lucky = 0
-        luckyNumberRef.on('value', function(a) {
-          var luckyNumberObjs = a.val();
-          for (var key in luckyNumberObjs) {
-            if (luckyNumberObjs.hasOwnProperty(key))
-              lucky = luckyNumberObjs[key].luckyNumber
-          }
-        });
-        var result = []
-        var diff = 0
-        lotteryRef.on('value', function(a) {
-          var lotteryObjs = a.val();
-          for (var key in lotteryObjs) {
-            if (lotteryObjs.hasOwnProperty(key)) {
-              diff = Math.abs(lotteryObjs[key].lotteryKey - lucky);
-              lotteryRef.child(key).update({
-                'diff': diff
-              })
-            }
-          }
-          lotteryRef.orderByChild("diff").once("value", function(winnerList) {
-            winnerList.forEach(function(data) {
-              if (result.length < 5) {
-
-                result.push( {
-                'wfid':data.val().wfid,
-                'lotteryKey':data.val().lotteryKey,
-                'diff':data.val().diff 
-               } )
-                winnersRef.push( {
-                'wfid':data.val().wfid,
-                'lotteryKey':data.val().lotteryKey,
-                'diff':data.val().diff 
-               } )
-              }
-            })
-          })
-
-          console.log(result)
-          
-        });
-
-
-
-
-
         if (!this.showTimeLeft) {
           alert("Lottery event has already passed. Please come back early next time.")
           return;
@@ -200,13 +156,62 @@
       removeLottery: function(Lottery) {
         lotteryRef.child(Lottery['.key']).remove()
         toastr.success('Lottery removed successfully')
+      },
+      getWinner: function() {
+        var lucky = 0
+        luckyNumberRef.on('value', function(a) {
+          var luckyNumberObjs = a.val();
+          for (var key in luckyNumberObjs) {
+            if (luckyNumberObjs.hasOwnProperty(key))
+              lucky = luckyNumberObjs[key].luckyNumber
+          }
+        });
+        var diff = 0
+        lotteryRef.on('value', function(a) {
+          var lotteryObjs = a.val();
+          for (var key in lotteryObjs) {
+            if (lotteryObjs.hasOwnProperty(key)) {
+              diff = Math.abs(lotteryObjs[key].lotteryKey - lucky);
+              lotteryRef.child(key).update({
+                'diff': diff
+              })
+            }
+          }
+          //get winner size
+          var winnersSize = 0
+          winnersRef.once("value", function(snapshot) {
+            winnersSize = snapshot.numChildren();
+          });
+          lotteryRef.orderByChild("diff").once("value", function(winnerList) {
+            winnerList.forEach(function(data) {
+              if (winnersSize < 5) {
+                winnersRef.push({
+                  'wfid': data.val().wfid,
+                  'lotteryKey': data.val().lotteryKey,
+                  'diff': data.val().diff
+                })
+              }
+            })
+          })
+        });
+      },
+      getLuckyNumber: function() {
+        var num = Math.floor((Math.random() * 1000) + 1);
+        console.log(num)
+        var luckyNumberdbSize = 0
+        luckyNumberRef.once("value", function(snapshot) {
+          luckyNumberdbSize = snapshot.numChildren();
+        });
+        if (luckyNumberdbSize == 0) {
+          luckyNumberRef.push({'luckyNumber':num})
+        }
       }
     },
     computed: {
       caltimeLeft: function() {
         var vm = this
         var left = ''
-        var countDownDate = new Date("June 30, 2017 20:36:40").getTime();
+        var countDownDate = new Date(this.eventEndTime).getTime();
         var x = setInterval(function() {
           var now = new Date().getTime();
           this.distance = countDownDate - now;
@@ -224,6 +229,14 @@
         }, 1000);
         return vm.timeLeft;
       },
+    },
+    watch: {
+      showTimeLeft: {
+        handler: function() {
+          this.getLuckyNumber()
+          this.getWinner()
+        }
+      }
     }
   }
 </script>
